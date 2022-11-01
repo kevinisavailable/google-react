f49c22d86ffb0f0923548297cee21e5b29d21838
Aravind Roy <aravindroy1709@gmail.com>
2022-11-01 12:13:47 +0530

    Navbar outline finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
    <>
-      <h1>Hello World</h1>
+      <Navbar />
+      <Main />
+      <Footer />
    </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.scss'
+const Navbar = () => {
+  return (
+   <>
+    <div className='navbar'>
+        <a href="#">Gmail</a>
+        <a href="#">Images</a>
+        <svg class="gb_Ve" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>
+        <img src="https://lh3.googleusercontent.com/ogw/AOh-ky0sBCMr85SWIMpDJ6aZjUFS5CX_mh0zm2WXQcjKrA=s32-c-mo" alt="" />
+    </div>
+   </>
+  )
+}
+
+export default Navbar
